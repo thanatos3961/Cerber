1ef76b7f3d73e68a5a5abbbf0256b8d87f05a60f
Levi Locklear <levilocklear410@gmail.com>
2024-10-01 17:28:20 -0400

    Update cogs file

--- cogs/empty.py
+++ cogs/empty.py
@@ -1,11 +1,19 @@
 import discord
 from discord.ext import commands
 
-class EmptyCog(commands.Cog):
+Denied_emoji = "<:WrenchDenied:1281769128633569330>"
+Checkmark_emoji = "<:WrenchCheckmark:1281769061763649536>"
+
+embed_color = discord.Color.red()
+unsuccessful_color = discord.Color.orange()
+
+class EMPTY(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-
-
+    
+    @commands.command(name="hi")
+    async def hello(self, ctx: commands.Context):
+        await ctx.reply("hi")
 
 async def setup(bot: commands.Bot):
-    await bot.add_cog(EmptyCog(bot))
+    await bot.add_cog(EMPTY(bot))
